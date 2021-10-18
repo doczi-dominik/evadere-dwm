22356890c0ff8a6db3e03f22d27118df7bbfbdff
doczi-dominik <doczi.dominik0@gmail.com>
2021-10-18 07:29:16 +0200

    🔧 Personalized keybinds

--- config.h
+++ config.h
@@ -89,47 +89,40 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      shiftmastersplit, {.i = +1} },   /* increase the number of tiled clients in the master area */
-	{ MODKEY,                       XK_d,      shiftmastersplit, {.i = -1} },   /* reduce the number of tiled clients in the master area */
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ControlMask,		XK_comma,  cyclelayout,    {.i = -1 } },
-	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} },
-	{ MODKEY|ControlMask,           XK_t,      rotatelayoutaxis, {.i = 0} },    /* 0 = layout axis */
-	{ MODKEY|ControlMask,           XK_Tab,    rotatelayoutaxis, {.i = 1} },    /* 1 = master axis */
-	{ MODKEY|ControlMask|ShiftMask, XK_Tab,    rotatelayoutaxis, {.i = 2} },    /* 2 = stack axis */
-	{ MODKEY|ControlMask,           XK_Return, mirrorlayout,     {0} },
+	{ MODKEY,                       XK_d,          spawn,            {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return,     spawn,            {.v = termcmd } },
+	{ MODKEY,                       XK_j,          focusstack,       {.i = +1 } },
+	{ MODKEY,                       XK_k,          focusstack,       {.i = -1 } },
+	{ MODKEY,                       XK_i,          shiftmastersplit, {.i = +1} },   /* increase the number of tiled clients in the master area */
+	{ MODKEY,                       XK_u,          shiftmastersplit, {.i = -1} },   /* reduce the number of tiled clients in the master area */
+	{ MODKEY,                       XK_h,          setmfact,         {.f = -0.05} },
+	{ MODKEY,                       XK_l,          setmfact,         {.f = +0.05} },
+	{ MODKEY,                       XK_Tab,        zoom,             {0} },
+	{ MODKEY,                       XK_q,          killclient,       {0} },
+	{ MODKEY,		                XK_comma,      cyclelayout,      {.i = -1 } },
+	{ MODKEY,                       XK_period,     cyclelayout,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_space,      togglefloating,   {0} },
+	{ MODKEY|ShiftMask,             XK_f,          togglefullscr,    {0} },
+	{ MODKEY,                       XK_0,          view,             {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,          tag,              {.ui = ~0 } },
+	{ MODKEY,                       XK_udiaeresis, focusmon,         {.i = -1 } },
+	{ MODKEY,                       XK_oacute,     focusmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_udiaeresis, tagmon,           {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_oacute,     tagmon,           {.i = +1 } },
+	TAGKEYS(                        XK_1,                            0)
+	TAGKEYS(                        XK_2,                            1)
+	TAGKEYS(                        XK_3,                            2)
+	TAGKEYS(                        XK_4,                            3)
+	TAGKEYS(                        XK_5,                            4)
+	TAGKEYS(                        XK_6,                            5)
+	TAGKEYS(                        XK_7,                            6)
+	TAGKEYS(                        XK_8,                            7)
+	TAGKEYS(                        XK_9,                            8)
+	{ MODKEY|ControlMask|ShiftMask, XK_q,          quit,             {1} },
+	{ MODKEY,                       XK_t,          rotatelayoutaxis, {.i = 0} },    /* 0 = layout axis */
+	{ MODKEY|ShiftMask,             XK_t,          rotatelayoutaxis, {.i = 1} },    /* 1 = master axis */
+	{ MODKEY|ControlMask,           XK_t,          rotatelayoutaxis, {.i = 2} },    /* 2 = stack axis */
+	{ MODKEY|ShiftMask|ControlMask, XK_t,          mirrorlayout,     {0} },
 };
 
 /* button definitions */
