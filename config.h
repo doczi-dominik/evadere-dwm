2e9d66231406cca36926a5e4243d4358346e23a3
doczi-dominik <doczi.dominik0@gmail.com>
2021-10-18 07:33:07 +0200

    🔧 Removed dmenu-specific monitor setting

--- config.h
+++ config.h
@@ -83,8 +83,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", background, "-nf", foreground, "-sb", accent, "-sf", sub, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", background, "-nf", foreground, "-sb", accent, "-sf", sub, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
